5024e80b410ab3eb3525ac7bd1695ad71734fa21
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 10:58:29 +0530

    Navbar Styled

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 				<div className='gradient' />
 			</div>
 
-			<div className='relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
+			<div className='relative z-10 flex justify-center items-center flex-col'>
 				<Navbar />
 				<Home />
 			</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<header className='w-full sticky top-0 backdrop-blur-lg'>
+			<nav className='w-full max-w-7xl mx-auto sm:px-16 px-6 py-4 flex justify-between items-center'>
+				<div>
+					<h1 className='font-bold text-xl'> Summarize </h1>
+				</div>
+
+				<button
+					type='button'
+					className='rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black'
+					onClick={() => window.open('https://www.github.com/anoopraju31')}>
+					GitHub
+				</button>
+			</nav>
+		</header>
+	)
 }
 
 export default Navbar
