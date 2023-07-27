2dc258aacb1674c9dd37b1bd8f68dbc25ef39acc
anoopraju31 <anoopraju31@gmail.com>
2023-07-28 02:55:24 +0530

    Data Fetching status added

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
 import { useAppSelector } from '../hooks'
-import { useLazyGetSummaryQuery } from '../services/articleApi'
 import { Loader } from './'
 
 const Summary = () => {
-	const summary = useAppSelector((state) => state.article.summary)
-	const [, { error, isFetching }] = useLazyGetSummaryQuery()
+	const { summary, status } = useAppSelector((state) => state.article)
 
 	return (
 		<section className='w-full max-w-7xl flex justify-center items-center'>
-			{isFetching ? (
+			{status === 'fetching' ? (
 				<Loader />
-			) : error ? (
+			) : status === 'error' ? (
 				<p className='font-inter font-bold text-black text-center'>
-					Well, that wasn't supposed to happen...
+					Well, something went wrong!
 				</p>
 			) : (
 				<div className='flex flex-col gap-3'>
-					<h2 className='mb-2 font-satoshi font-bold text-center text-gray-700 text-xl'>
-						Article{' '}
-						<span className='font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent'>
-							Summary
-						</span>
-					</h2>
+					{summary && (
+						<h2 className='mb-2 font-satoshi font-bold text-center text-gray-700 text-xl'>
+							Article{' '}
+							<span className='font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent'>
+								Summary
+							</span>
+						</h2>
+					)}
 					<div className='p-4 md:px-6 bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10'>
 						<p className='font-inter font-medium text-sm text-gray-700'>
 							{summary}
