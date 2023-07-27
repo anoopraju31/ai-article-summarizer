840adb9faf29cdf69f1f28f908d259cffb282d64
anoopraju31 <anoopraju31@gmail.com>
2023-07-28 00:00:01 +0530

    url container component - logic added

--- src/components/UrlContainer.tsx
+++ src/components/UrlContainer.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
 import { GoCopy } from 'react-icons/go'
+import { TiTick } from 'react-icons/ti'
+import { useAppDispatch } from '../hooks'
+import { addSummary, addUrl } from '../services/article'
 
 interface article {
 	url: string
@@ -10,15 +14,33 @@ type UrlContainerProps = {
 }
 
 const UrlContainer = ({ item }: UrlContainerProps) => {
+	const [copied, setCopied] = useState<string>('')
+	const dispatch = useAppDispatch()
+
+	const handleClick = () => {
+		dispatch(addUrl(item.url))
+		dispatch(addSummary(item.summary))
+	}
+
+	const handleCopy = (copyUrl: string) => {
+		setCopied(copyUrl)
+		navigator.clipboard.writeText(copyUrl)
+		setTimeout(() => setCopied(''), 3000)
+	}
+
 	return (
-		<div className='group py-2 px-3 flex justify-start items-center flex-row gap-5 bg-white rounded-lg cursor-pointer hover:bg-transparent hover:text-gray-700 hover:shadow-sm transition-all duration-1000'>
+		<div
+			onClick={handleClick}
+			className='group py-2 px-3 flex justify-start items-center flex-row gap-5 bg-white rounded-lg cursor-pointer hover:bg-transparent hover:text-gray-700 hover:shadow-sm transition-all duration-1000'>
 			<p className='flex-1 font-serif text-gray-600 group-hover:text-orange-400 font-medium text-sm truncate transition-colors duration-1000'>
 				{item.url}
 			</p>
 
 			<div className='w-7 h-7 bg-gray-50 group-hover:bg-white rounded-full backdrop-blur cursor-pointer'>
-				<div className='w-full h-full flex justify-center items-center rounded-full hover:bg-orange-500 hover:text-white transition-colors duration-1000'>
-					<GoCopy />
+				<div
+					onClick={() => handleCopy(item.url)}
+					className='w-full h-full flex justify-center items-center rounded-full hover:bg-orange-500 hover:text-white transition-colors duration-1000'>
+					{copied !== item.url ? <GoCopy /> : <TiTick />}
 				</div>
 			</div>
 		</div>
