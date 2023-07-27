8375d6377178333a06a3557b9fa067a44a7196f2
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 11:42:53 +0530

    Navbar restyled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,15 @@ const Navbar = () => {
 		<header className='w-full sticky top-0 backdrop-blur-lg'>
 			<nav className='w-full max-w-7xl mx-auto sm:px-16 px-6 py-4 flex justify-between items-center'>
 				<div>
-					<h1 className='font-bold text-xl'> Summarize </h1>
+					<h1 className='font-bold text-xl bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent'>
+						{' '}
+						Summarize{' '}
+					</h1>
 				</div>
 
 				<button
 					type='button'
-					className='rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black'
+					className='rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:bg-gradient-to-r hover:from-amber-500 hover:via-orange-600 hover:to-yellow-500 hover:bg-clip-text hover:text-transparent hover:border-transparent'
 					onClick={() => window.open('https://www.github.com/anoopraju31')}>
 					GitHub
 				</button>
