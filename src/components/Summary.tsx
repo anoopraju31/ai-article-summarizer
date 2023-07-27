791d2f0361ede9d4f1f5aecba4eb458fa7d57a84
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 12:49:19 +0530

    Summary intial styling

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
 
 const Summary = () => {
-	return <div>Summary</div>
+	return (
+		<section className='w-full max-w-7xl flex justify-center items-center'>
+			<div className='flex flex-col gap-3'>
+				<h2 className='mb-2 font-satoshi font-bold text-center text-gray-700 text-xl'>
+					Article{' '}
+					<span className='font-black bg-gradient-to-r from-blue-600 to-cyan-600 bg-clip-text text-transparent'>
+						Summary
+					</span>
+				</h2>
+				<div className='py-4 px-6 bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10'>
+					<p className='font-inter font-medium text-sm text-gray-700 text-justify'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+						ratione ab alias voluptatem error, doloribus id quae nostrum dicta
+						laudantium iure excepturi. Qui nisi aliquid dignissimos non
+						laudantium vero exercitationem fugit aperiam similique, quae quidem
+						animi dolor facilis. Sequi ipsa expedita cum, odio praesentium
+						voluptatibus saepe maiores adipisci vitae nobis distinctio minus
+						fuga voluptatem tenetur dolor ea consequuntur quas laboriosam,
+						magnam itaque iure? Porro repellendus veniam architecto explicabo
+						optio aspernatur fuga quos inventore hic adipisci rem saepe id
+						doloribus, neque nostrum ratione. Exercitationem necessitatibus,
+						quaerat dolorem qui, pariatur quia illo soluta reprehenderit,
+						voluptatibus iusto omnis cum corrupti molestias! Ullam placeat quae
+						pariatur maiores minima ducimus odit tenetur consequuntur voluptate
+						dicta debitis necessitatibus culpa accusantium aspernatur, dolores
+						ipsum quasi quia quisquam? Quam iusto consectetur laudantium
+						provident cumque aperiam impedit. Voluptatibus, error veniam
+						molestiae est dolore placeat similique rem laborum aperiam eaque
+						commodi quo eos ut iusto expedita? Ex corrupti deserunt reiciendis
+						quos consequatur culpa. Sequi eius possimus, dolore cumque error
+						incidunt quisquam voluptates soluta optio aperiam eos reprehenderit
+						facilis natus labore aliquam exercitationem recusandae perspiciatis
+						saepe! Explicabo repudiandae eligendi eum accusamus, sint eius
+						molestias quaerat exercitationem nam, a enim maiores consectetur
+						omnis in tenetur vitae non sequi architecto libero unde ad?
+					</p>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Summary
