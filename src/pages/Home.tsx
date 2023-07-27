7f8e73638e9d9077d0b253f8eabd60faad1f30bf
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 11:49:18 +0530

    Form initial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Hero } from '../components'
+import { Form, Hero } from '../components'
 
 const Home = () => {
 	return (
 		<div className='w-full max-w-7xl min-h-screen mx-auto sm:px-16 px-6'>
 			<Hero />
+			<Form />
 		</div>
 	)
 }
