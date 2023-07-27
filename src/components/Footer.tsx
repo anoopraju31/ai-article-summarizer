48b12e7999b4019a70eb706cabb09d1d7382f40b
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 11:14:56 +0530

    Footer Styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='w-full max-w-7xl mx-auto sm:px-16 px-6'>
+			<div className='w-full my-6 lg:my-8'>
+				<hr className='mb-6 border-gray-400 lg:mb-8' />
+				<span className='block text-sm text-gray-700 text-center leading-6'>
+					© 2023{' '}
+					<a href='https://flowbite.com/' className='hover:underline'>
+						Summarize™
+					</a>
+					. <br className='block sm:hidden' />
+					All Rights Reserved.
+				</span>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
