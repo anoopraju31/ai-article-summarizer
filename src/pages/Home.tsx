60162bf16bc0ae89bc10939ca2b21c1f7cab446a
anoopraju31 <anoopraju31@gmail.com>
2023-07-27 11:30:08 +0530

    Hero section initial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Hero } from '../components'
 
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<div className='w-full max-w-7xl min-h-screen mx-auto sm:px-16 px-6'>
+			<Hero />
+		</div>
+	)
 }
 
 export default Home
